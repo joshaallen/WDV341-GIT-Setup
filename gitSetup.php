dc87d435d3193ab7bd3ffaceb22c7867cc9b3e6f
joshaallen <jaalle10@dmacc.edu>
2021-08-29 13:12:27 -0500

    added css styling

---
+++ gitSetup.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+         <!--
+				Unit 1 Assignment
+				Author: Joshua Allen
+				Date: September 29, 2021
+	-->
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Homework assignment explaining GIT technology through application of PHP in the form of a web page">
+    <meta name="keywords" content="PHP, GIT, Homework, Version Control">
+    <meta name="author" content="Joshua Allen">
+    <title>Git Code Repository Assignment</title>
+    <link rel="stylesheet" type="text/css" href="gitSetup.css">
+</head>
+<body>
+    <div class="container">
+        <header><h1>Download Git Client & Create a Repository account Assignment</h1></header>
+        <aside>
+            <ul>
+                <li><a href="#"></a>Home</li>
+                <li><a href="#"></a>Homework Page</li>
+                <li><a href="#"></a>My Blog</li>
+                <li><a href="#"></a>SCSS</li>
+            </ul>
+        </aside>
+        <main>
+            <p>My name is: <?php echo "Joshua Allen";?></p>  
+            <p>Define the following Git related terms: </p>
+            <dl>
+                <dt>Version control software</dt>
+                <dd>A system that records changes to a file or set of files over time so that you can recall specific versions later.</dd>
+                <dt>Add</dt>
+                <dd>This command updates the index using the current content found in the working tree, to prepare the content staged for the next commit.</dd>
+                <dt>Commit</dt>
+                <dd>Record changes to the repository.</dd>
+                <dt>Push</dt>
+                <dd>Updates remote refs using local refs, while sending objects necessary to complete the given refs.</dd>
+                <dt>Pull</dt>
+                <dd>Fetch from and integrate with another repository or a local branch</dd>
+                <dt>Clone</dt>
+                <dd>Clones a repository into a newly created directory, creates remote-tracking branches for each branch in the cloned repository.</dd>
+            </dl>
+        </main>
+    </div><!--end of container-->
+</body>
+</html>
